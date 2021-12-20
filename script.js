8b6cb1502f5c495db375a2c261e4b70d7ea91772
allexkid <allexsantosrn@gmail.com>
2021-12-20 20:40:41 -0300

    Finalização do JavaScript
    Finalização do JavaScript.

--- script.js
+++ script.js
@@ -6,12 +6,57 @@ const peso = document.getElementById('peso');
 const calcular = document.getElementById('calcular');
 const resultado = document.getElementById('resultado');
 
-/* Acessando algum objeto - Escrevendo no input nome */
-//nome.value = 'Alexandre Dantas';
+/* Acessando algum objeto - Escrevendo no input nome. */
+// nome.value = 'Alexandre Dantas';
 
-/*Adicionando listener no clique do botão */
+/* Adicionando listener no clique do botão. */
 calcular.addEventListener('click', imc);
 
-function imc {
-    alert ('Test click');
+function imc() {
+    /* Teste de Clique. */
+    // alert ('Test click');
+
+    if (nome.value !== '' && peso.value !== '' && altura.value !== '') {
+        //alert('valor preenchido!');
+
+        /* Criando variável para cálculo do resultado. Função toFixed(2) realiza o arredondamento para duas casas decimais */
+        const valorImc = (peso.value / (altura.value * altura.value)).toFixed(2);
+
+        let classificacao = "";
+
+        if (valorImc < 18.5) {
+            classificacao = 'abaixo do peso.';
+        }
+
+        else if (valorImc < 25) {
+            classificacao = 'com peso ideal. Parabéns!!!';
+        }
+
+        else if (valorImc < 30) {
+            classificacao = 'levemente acima do peso.';
+        }
+
+        else if (valorImc < 35) {
+            classificacao = 'com obesidade grau I.';
+        }
+
+        else if (valorImc < 40) {
+            classificacao = 'com obesidade grau II.';
+        }
+
+        else {
+            classificacao = 'com obesidade grau III. Cuidado!!!';
+        }
+
+        /* Escrevendo o resultado na saída. */
+        //resultado.textContent = valorImc;
+
+        resultado.textContent = `${nome.value}, seu IMC é de ${valorImc} e você está ${classificacao}`;
+    }
+
+    else {
+        /* Escrevendo exceção caso os campos estejam vazios. */
+        resultado.textContent = 'Preencha todos os campos!!!';
+    }
+
 }
